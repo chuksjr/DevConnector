7b6c0c67e83ab52e6752b0899bf90b1e3a3f99a0
Chuks Ricardo Iloegbunam <chuksricardo@gmail.com>
2021-01-13 16:51:11 -0300

    Adding dependencies and creating routes
    Adding dependencies, creating routes for users, profiles, auth and posts and setting up database connection.

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express");
-
+const { connect } = require("mongoose");
+const connectDB = require("./config/db");
 const app = express();
 
+//connect database
+connectDB();
 app.get("/", (req, res) => res.send("API Running"));
+
+//Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 const PORT = process.env.PORT || 5000;
 
